e4f2b08ba3f1867b086170e8624429c8193360cd
Chase <chaseit12@gmail.com>
2018-04-05 21:38:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,11 +15,7 @@
      })
    }
    
-   function frameIt() {
-  $('img').on('load', function() {
-    $('img').addClass('tasty')
-  })
-}
+ 
    
    function pressIt(){
      $('input').on('keydown', ()=>{
