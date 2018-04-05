c9a36d98d274908f4862f8f7a7808d76effbb1fe
Chase <chaseit12@gmail.com>
2018-04-05 21:41:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@
    
    
    function pressIt(){
-     $('#typing').on('keydown', ()=>{
+     $('#typing').on('keydown', (e)=>{
       if (e.which === 71) {
         alert("You pressed 'g'!")
        }
